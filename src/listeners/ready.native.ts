372e482774435358e2d3cb381afc5aea07d3f0d6
Nirbose <nirbose2@gmail.com>
2022-01-13 22:19:59 +0100

    run music
    Peut jouer de la musique en boucle

--- src/listeners/ready.native.ts
+++ src/listeners/ready.native.ts
@@ -1,10 +1,13 @@
 import figlet from "figlet"
 import boxen from "boxen"
 import chalk from "chalk"
+import ytdl from "ytdl-core"
+import yts from "yt-search"
 
 import * as app from "../app.js"
 
 import { filename } from "dirname-filename-esm"
+import { AudioPlayer, AudioPlayerStatus, createAudioPlayer, createAudioResource, DiscordGatewayAdapterCreator, joinVoiceChannel, NoSubscriberBehavior } from "@discordjs/voice"
 
 const __filename = filename(import.meta)
 
@@ -12,7 +15,7 @@ const listener: app.Listener<"ready"> = {
   event: "ready",
   description: "Just log that bot is ready",
   once: true,
-  async run() {
+  async run(client) {
     app.log(
       `Ok i'm ready! ${chalk.blue(
         "My default prefix is"
@@ -37,6 +40,52 @@ const listener: app.Listener<"ready"> = {
           },
         })
       )
+    })
+
+    const keys = {
+      music: ["pop emd", "tendence", "musique", "danse"]
+    }
+
+    client.guilds.cache.forEach(guild => {
+      guild.channels.cache.forEach(async (channel) => {
+        if (channel.id === "818590955677417515") {
+          if (channel.isVoice()) {
+            if (channel instanceof app.VoiceChannel) {
+              let join = joinVoiceChannel({
+                channelId: channel.id,
+                guildId: guild.id,
+                adapterCreator: channel.guild.voiceAdapterCreator as unknown as DiscordGatewayAdapterCreator,
+              })
+
+              const player = createAudioPlayer({
+                behaviors: {
+                  noSubscriber: NoSubscriberBehavior.Pause,
+                },
+              })
+
+              const init = async () => {
+                const videos = await yts(keys.music[Math.floor(Math.random() * keys.music.length)])
+                videos.videos.forEach(async (video, index) => {
+                  if (video.duration.seconds !> 300) {
+                    const resource = createAudioResource(ytdl(video.url))
+                    player.play(resource)
+
+                    join.subscribe(player)
+                  } else {
+                    console.log(`${index} | ${video.title}`)
+                  }
+                })
+              }
+
+              player.on(AudioPlayerStatus.Idle, () => {
+                init()
+              })
+
+              init()
+            }
+          }
+        }
+      })
     })
   },
 }
